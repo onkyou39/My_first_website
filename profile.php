478b44a652732c64494e449d986eff65b6b8627d
onkyou39 <as.1997.00@gmail.com>
2024-05-02 17:22:02 +0300

    Add login and post forms

--- profile.php
+++ profile.php
@@ -35,9 +35,27 @@
         </div>
     </div>
     <div class="container">
-        <div class="button_js col-12">
-            <button id="myButton">Click me</button>
-            <p id="demo"></p>
+        <div class="row">
+            <div class="button_js col-12">
+                <button id="myButton">Click me</button>
+                <p id="demo"></p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row comment_section">
+            <div class="col-12">
+                <h1 class="hello">
+                    Привет, <?php echo $_COOKIE['User']; ?>
+                </h1>
+            </div>
+            <div class="col-12">
+                <form method="POST" action="profile.php" class="form-container">
+                    <input type="text" class="form" type="text" name="title" placeholder="Заголовок вашего поста">
+                    <textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста ..."></textarea>
+                    <button type="submit" class="btn_yellow" name="submit">Сохранить пост!</button>
+                </form>
+            </div>
         </div>
     </div>
     <div class="footer">
@@ -50,3 +68,21 @@
 <script type="text/javascript" src="js/button.js"></script>
 </body>
 </html>
+
+<?php
+require_once('db.php');
+
+$link = mysqli_connect('127.0.0.1', 'root', 'kali', 'first');
+
+if (isset($_POST['submit'])) {
+
+    $title = $_POST['title'];
+    $main_text = $_POST['text'];
+
+    if (!$title || !$main_text) die("Заполните все поля");
+
+    $sql = "INSERT INTO posts (title, main_text) VALUES ('$title', '$main_text')";
+
+    if(!mysqli_query($link, $sql)) die("Не удалось добавить пост");
+}
+?>
